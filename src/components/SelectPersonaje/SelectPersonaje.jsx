2c774ddc62588a82a815c9e61d47cd4cfdd95fe2
ElDyrke <degs1212@gmail.com>
2024-11-12 16:29:22 -0300

    Menu Biblioteca de Personajes

--- src/components/SelectPersonaje/SelectPersonaje.jsx
+++ src/components/SelectPersonaje/SelectPersonaje.jsx
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import "./SelectPersonaje.css"
 import PersonajeDefault from '../../personajeDefault'
+import 'bootstrap/dist/css/bootstrap.css'; 
+import Offcanvas from 'react-bootstrap/Offcanvas'
+import { List } from 'react-bootstrap-icons';
+
 
 const SelectPersonaje = ({personaje, pjIndex, setIndex}) => {
   if (JSON.parse(localStorage.getItem("personajes"))){
@@ -10,15 +14,48 @@ const SelectPersonaje = ({personaje, pjIndex, setIndex}) => {
     var nombres = [PersonajeDefault.infoBase.nombre, PersonajeDefault.infoBase.nombre, PersonajeDefault.infoBase.nombre] 
   }
 
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
 
   return (
+    <>
     <div className='SelectPersonaje'>
-      <select>
-        <option onClick={() => setIndex(0)}>{nombres[0]}</option>
-        <option onClick={() => setIndex(1)}>{nombres[1]}</option>
-        <option onClick={() => setIndex(2)}>{nombres[2]}</option>
-      </select>
+      <button onClick={handleShow}><List/></button>
     </div>
+    
+    <DisplayPersonajes 
+    nombres={nombres} 
+    setIndex={setIndex} 
+    show={show}
+    handleClose={handleClose}
+    pjIndex={pjIndex}
+    />
+    </>
+  )
+}
+
+const DisplayPersonajes = ({nombres, setIndex, pjIndex, show, handleClose}) => {
+  return (
+    <Offcanvas show={show} onHide={handleClose}>
+      <Offcanvas.Header closeButton>
+        <span className="display-5">Biblioteca de Personajes</span>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <ul>
+          {
+            nombres.map((v, i) => {
+            if (i === pjIndex) {
+              return  <li key={i} onClick={() => setIndex(i)} className='selected'>{v}</li>
+            }
+            return  (
+              <li key={i} onClick={() => setIndex(i)}>{v}</li>
+            )
+          })
+          }
+        </ul>
+      </Offcanvas.Body>
+    </Offcanvas>
   )
 }
 
